c27ba5b1cfee5bd7ce271eea89e4d37a127714ad
Fabricio Dourado <fcvieira2@gmail.com>
2022-05-24 22:28:22 -0300

    Add migrations for DB

---
+++ database/migrations/2022_05_25_012308_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('references');
+            $table->string('description');
+            $table->string('brand');
+            $table->float('price', 8, 2);
+            $table->decimal('quantity_in_stock', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
