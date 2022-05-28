2e7258ab8d067116f065dc352176f4df5301040e
Fabricio Dourado <fcvieira2@gmail.com>
2022-05-28 17:01:32 -0300

    Adicionando regra para Cors

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('v1')->group(function () {
+Route::middleware('cors')->prefix('v1')->group(function () {
     Route::resource('/client', ClientController::class, ['only' => ['index' ,'store']]);
 });
 
